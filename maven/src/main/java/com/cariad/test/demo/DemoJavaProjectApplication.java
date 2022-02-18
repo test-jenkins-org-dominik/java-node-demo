2c4826a4131f9dc6569d91d54dbc8d20b742d1dc
Dominik Jurkovic <dominikj008@gmail.com>
2022-02-18 10:11:27 +0100

    Add files via upload

---
+++ maven/src/main/java/com/cariad/test/demo/DemoJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cariad.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaProjectApplication.class, args);
+	}
+
+}
---
+++ maven/src/test/java/com/cariad/test/demo/DemoJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cariad.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
